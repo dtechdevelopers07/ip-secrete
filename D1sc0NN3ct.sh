83f74f68f76eba580bf2e7b4c65627f81f4e22fd
dtechdevelopers07 <55192607+dtechdevelopers07@users.noreply.github.com>
2020-10-21 14:48:55 +0500

    Update D1sc0NN3ct.sh

--- D1sc0NN3ct.sh
+++ D1sc0NN3ct.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-response=$(curl -k -d "$SH_DATA&type=1" -X POST $API_LINK) echo $response;
+response=$(curl -k -d "$SH_DATA&type=1&download=$STATS_BYTES_OUT&upload=$STATS_BYTES_IN&duration=$STATS_DURATION&real_ip=$IP_REAL&ip_remote=$IP_REMOTE" -X POST $API_LINK) echo $response;
 exit 0;
